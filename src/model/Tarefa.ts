af900b13c7e8359a77883edde671fbe182479fd2
LuizFXavier <zap422238@gmail.com>
2023-09-04 22:57:17 -0400

    Implementação do Model

---
+++ src/model/Tarefa.ts
@@ -0,0 +1,36 @@
+import db from "../database/connection"
+
+class Tarefa {
+
+    id: number;
+    titulo: string;
+    data_de_conclusao: Date;
+    data_de_criacao: Date;
+    concluida: boolean;
+    id_usuario: number;
+
+    constructor(task: { id: number | null, titulo: string, data_de_conclusao: Date, data_de_criacao: Date, concluida: boolean, id_usuario: number }){
+        
+        this.id = task.id ?? 0
+        this.titulo = task.titulo
+        this.data_de_conclusao = task.data_de_conclusao
+        this.data_de_criacao = task.data_de_criacao
+        this.concluida = task.concluida
+        this.id_usuario = task.id_usuario
+    }
+
+    public async create() {
+        await db.query(`
+
+            CREATE TABLE tarefa (
+                id SERIAL PRIMARY KEY UNIQUE,
+                titulo VARCHAR,
+                data_de_conclusao TIMESTAMP,
+                data_de_criacao TIMESTAMP,
+                concluida BOOLEAN,
+                id_usuario INTEGER
+                FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
+            );
+        `).then(() => console.log("Tabela tarefa criada com sucesso"))
+    }
+}
---
+++ src/model/Usuario.ts
@@ -0,0 +1,29 @@
+import db from "../database/connection"
+
+class Usuario {
+
+    id: number;
+    nome: string;
+    e_mail: string;
+    senha: string;
+
+    constructor(user:{id: number | null, nome: string, e_mail: string, senha: string}) {
+        this.id = user.id ?? 0
+        this.nome = user.nome
+        this.e_mail = user.e_mail
+        this.senha = user.senha
+    }
+
+    public async create() {
+        await db.query(`
+            CREATE TABLE usuario (
+                id SERIAL PRIMARY KEY,
+                nome VARCHAR,
+                e_mail VARCHAR,
+                senha VARCHAR
+            ');
+        `).then(() => console.log("Tabela usuário criada com sucesso"))
+    }
+}
+
+export default Usuario
